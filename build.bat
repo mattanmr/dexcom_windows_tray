de3e9ca63b1896098126707512a8cf05b10e6f15
mattanmr <61776185+mattanmr@users.noreply.github.com>
2025-07-21 08:30:23 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 @echo off
+REM Install dependencies
+pip install -r requirements.txt
+REM Build executable
 pip install pyinstaller
-pyinstaller --noconsole --onefile dexcom_tray_secure.py
+python -m PyInstaller --noconsole --onefile --hidden-import win10toast --hidden-import pkg_resources --collect-all win10toast dexcom_tray_secure.py
 pause
